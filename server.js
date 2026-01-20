1f25a2d77d0f5cd2db5884af8d3d88f22caa109f
kong <tee0918831415@gmail.com>
2026-01-21 02:34:39 +0700

    library fix

--- server.js
+++ server.js
@@ -1,57 +1,61 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
-const session = require('express-session');
-const auth = require('./middleware/auth');
+  require('dotenv').config();
+  const express = require('express');
+  const cors = require('cors');
+  const connectDB = require('./config/db');
+  const session = require('express-session');
+  const auth = require('./middleware/auth');
 
-const authRoutes = require("./routes/authRoutes");
-const bookRoutes = require("./routes/bookRoutes");
+  const authRoutes = require("./routes/authRoutes");
+  const bookRoutes = require("./routes/bookRoutes");
 
-const app = express();
+  const app = express();
 
-connectDB();
+  connectDB();
 
-/* 🔥 CORS (ต้องมาก่อน session) */
-app.use(cors({
-  origin: "http://localhost:2000",
-  credentials: true
-}));
+  /* 🔥 CORS (ต้องมาก่อน session) */
+  app.use(cors({
+    origin: "http://localhost:2000",
+    credentials: true
+  }));
 
-/* body parser */
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+  /* body parser */
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
-/* 🔥 SESSION */
-app.use(session({
-  name: "connect.sid",
-  secret: "admin-secret-key",
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    httpOnly: true,
-    secure: false,      // localhost
-    sameSite: "lax"
-  }
-}));
+  /* 🔥 SESSION */
+  app.use(session({
+    name: "connect.sid",
+    secret: "admin-secret-key",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,      // localhost
+      sameSite: "lax"
+    }
+  }));
 
-/* 🔥 Static (ปิด index.html อัตโนมัติ) */
-app.use(express.static('public', {
-  index: false
-}));
+  /* 🔥 Static (ปิด index.html อัตโนมัติ) */
+  app.use(express.static('public', {
+    index: false
+  }));
 
-/* components */
-app.use("/components", express.static("components"));
+  /* components */
+  app.use("/components", express.static("components"));
 
-/* routes */
-app.use("/api/auth", authRoutes);
-app.use("/api/books", bookRoutes);
+  /* routes */
+  app.use("/api/auth", authRoutes);
+  app.use("/api/books", bookRoutes);
 
-/* 🔒 Protected page */
-app.get('/', auth, (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  /* 🔒 Protected page */
+  app.get('/', auth, (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+  });
+
+  app.get('/library', auth, (req, res) => {
+  res.sendFile(__dirname + '/public/library.html');
 });
 
-app.listen(2000, () => {
-  console.log("Server running at http://localhost:2000");
-});
+  app.listen(2000, () => {
+    console.log("Server running at http://localhost:2000");
+  });
